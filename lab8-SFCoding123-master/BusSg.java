6584280caf29f1c84466c1d41788c452f6d77758
Wang Shaofeng <47636365+SFCoding123@users.noreply.github.com>
2022-05-22 16:38:48 +0800

    Add files via upload

---
+++ lab8-SFCoding123-master/BusSg.java
@@ -0,0 +1,30 @@
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.stream.Collectors; 
+
+/**
+ * A BusSg class encapsulate the data related to the bus services and
+ * bus stops in Singapore, and supports queries to the data.
+ *
+ * @author: Ooi Wei Tsang
+ * @version: CS2030S AY21/22 Semester 2, Lab 8
+ */
+class BusSg {
+
+  /**
+   * Given a bus stop and a name, find the bus services that serve between
+   * the given stop and any bus stop with matching mame.
+   * @param  stop The bus stop.  Assume to be not null.
+   * @param  searchString The (partial) name of other bus stops, assume not null.
+   * @return The (optional) bus routes between the stops.
+   */
+  public static CompletableFuture<BusRoutes> findBusServicesBetween(BusStop stop,
+      String searchString) {
+    return stop.getBusServices().thenApply(input -> input.stream()
+        .collect(Collectors.toMap(service -> service, 
+            service -> service.findStopsWith(searchString))))
+      .thenApply(validServices -> new BusRoutes(stop, searchString, validServices));
+  }
+}
